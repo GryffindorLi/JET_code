37a8644083ec01f5e199330b194ec1e3b59ba86c
GryffindorLi <40144709+GryffindorLi@users.noreply.github.com>
2024-04-18 11:35:40 +0800

    fix bugs in main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ The entrance of the Restaurants query program.
 """
 import json
 import logging
+from functools import lru_cache
+from typing import Dict, List, Optional
 
 import requests
-from functools import lru_cache
 
 from src.parser import build_parser, split_input
 from src.table_printer import TablePrinter
@@ -22,43 +23,56 @@ def build_logger(to_file: bool = True, to_console: bool = True) -> logging.Logge
     Returns:
         a instance of logging.Logger class.
     """
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
+    lgr = logging.getLogger(__name__)
+    lgr.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
     if to_console:
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.INFO)
         console_handler.setFormatter(formatter)
-        logger.addHandler(console_handler)
+        lgr.addHandler(console_handler)
 
     if to_file:
         file_handler = logging.FileHandler('logfile.log')
         file_handler.setLevel(logging.INFO)
         file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+        lgr.addHandler(file_handler)
 
-    return logger
+    return lgr
+
 
 logger = build_logger()
 
+
 @lru_cache(maxsize=64)
-def get_data(postcode: str) -> Optional[Dict]:
+def get_data(postcode: str) -> Optional[List[Dict]]:
+    """
+    a function that fetches data from the API and returns all metadata of the first 10 restaurants.
+    Use lru_cache to reduce API calls for recently called postcodes.
+    Args:
+        postcode: a str represents the postcode.
+    Returns:
+        an Optional[Dict]. If fetch successfully from API or from lru_cache,
+        return a list of json dict. Otherwise, return None.
+    """
     if not check_postcode(postcode):
-        logger.error(f"Wrong postcode {args.search}. "
+        logger.error(f"Wrong postcode {postcode}. "
                      "Please provide a valid UK postcode")
         return
     url = "https://uk.api.just-eat.io/discovery/uk/restaurants/"\
-           f"enriched/bypostcode/{args.search}"
-    response = requests.get(url=url, timeout=2.0)
+        f"enriched/bypostcode/{postcode}"
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) '
+               'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 '
+               'Safari/537.36'}
+    response = requests.get(url=url, timeout=2.0, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
         logger.info(f"Successfully fetching data for {postcode}")
         return data['restaurants'][:10]
-    else:
-        logger.warning(f"Fail to fetch data for {postcode}. Status code {response.status_code}")
-        return None
+    logger.warning(f"Fail to fetch data for {
+                   postcode}. Status code {response.status_code}")
 
 
 def main(debug: bool = False):
@@ -84,7 +98,6 @@ def main(debug: bool = False):
                          "Unrecognized argument. Please check your "
                          "input arguments, or use '-h' for help.")
             continue
-        # args = parser.parse_args(split_input(cmd))
 
         if args.quit:
             logger.info("Quiting the program......")
@@ -105,4 +118,4 @@ def main(debug: bool = False):
 
 
 if __name__ == "__main__":
-    main(debug=True)
+    main(debug=False)
